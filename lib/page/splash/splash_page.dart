82e503f2476a4ea283e9761f8d1a2a6a72986450
nangle <nangle>
2022-08-31 12:56:08 +0700

    add resources, update splash page

--- lib/page/splash/splash_page.dart
+++ lib/page/splash/splash_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,15 +21,65 @@ class SplashPageState extends ConsumerState<SplashPage> {
 
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            AnimLottieCommon(
-              height: 200,
-              width: 200,
-              asset: 'assets/raws/anim_splash_meditation.json',
-            )
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: ExactAssetImage(
+                          'assets/images/img_moon_light_1.png',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 5,
+                        sigmaY: 5,
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedTextKit(
+                  onFinished: () {},
+                  animatedTexts: [
+                    RotateAnimatedText(
+                      '',
+                      duration: const Duration(
+                        seconds: 2,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  'Meditation App',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                const Center(
+                  child: AnimLottieCommon(
+                    height: 200,
+                    width: 200,
+                    asset: 'assets/raws/anim_splash_meditation.json',
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
